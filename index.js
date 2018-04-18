4903623fa96034db2e08cbc74fe98e7e6a8eadf2
Mike C <mikecronin92@gmail.com>
2018-04-18 03:12:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 function breakOut(array, changeValue, stopValue) {
   for (let i=0;i<array.length;i++) {
-    if (array[i] !== stopValue) {
-      array[i] = changeValue;
-    } else {
-      break;
+    if (array[i] == stopValue) {
+      break
     }
+    array[i] = changeValue
   }
   return array;
+}
+function breakOut(array, changeValue, stopValue) {
+  for (let i = 0, l = array.length; i < l; i++) {
+    if (array[i] === stopValue) {
+      break
+    }
+    array[i] = changeValue
+  }
+  return array
 }
 
 function keepGoing(array, changeValue, skipValue) {
@@ -27,4 +35,30 @@ function findBy(array, findFn) {
     }
   }
   return null;
+}
+
+////////////
+
+
+
+function keepGoing(array, changeValue, skipValue) {
+  for (let i = 0, l = array.length; i < l; i++) {
+    if (array[i] === skipValue) {
+      continue
+    }
+
+    array[i] = changeValue
+  }
+
+  return array
+}
+
+function findBy(array, findFn) {
+  for (let i = 0, l = array.length; i < l; i++) {
+    if (findFn(array[i])) {
+      return array[i]
+    }
+  }
+
+  return null
 }
